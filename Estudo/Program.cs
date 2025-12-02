1e1de4f170744c664c2c6886673318fc16a9eede
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 15:23:48 -0300

    Add project files.

---
+++ Estudo/Program.cs
@@ -0,0 +1,11 @@
+﻿// This line prints "Hello, World!"
+Console.WriteLine("Hello, World!");
+
+
+/*
+ *   Quem estuda ghello world se da muito bem na programação
+ *   por isso que estou voltando para estudar programação
+ *   estudart programção d everdade pela documentaçõ, que é a melhor fonte de estudo
+ *   que pe muiot importnae inves de aprender naquele curso, mas o senac foi otimo 
+ *   aprendi muito e eovluir muito, 
+ */
