499796da1641fa07075f55c5ed631fea5bf22f78
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 16:06:29 -0300

    Desenvolvimento paar evoluir profissionalmente, pois a prendizado é poder e evolução sempre!

---
+++ Estudo00/Entidade/Valor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo00.Entidade
+{
+    public class Valor
+    {
+        public static bool Or(bool left, bool right) => left || right;
+        public static bool And(bool left, bool right) => left && right;
+        public static bool Xor(bool left, bool right) => left ^ right;
+    }
+}
---
+++ Estudo00/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Testando operações lógicas no .NET 8:");
+
+bool Or(bool left, bool right) => left || right;
+bool And(bool left, bool right) => left && right;
+bool Xor(bool left, bool right) => left ^ right;
+
+Console.WriteLine($"Or(true, false) = {Or(true, false)}");
+Console.WriteLine($"And(true, false) = {And(true, false)}");
+Console.WriteLine($"Xor(true, false) = {Xor(true, false)}");
