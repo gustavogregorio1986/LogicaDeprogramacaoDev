ebafb1d1a6342f873e0f3f6c173cdb53d9975454
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 18:12:48 -0300

    Logica de programação paar evoluir como profissional e ser bem sucedido

---
+++ Estudo4/Control/ControlaValor.cs
@@ -0,0 +1,24 @@
+﻿using Estudo4.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo4.Control
+{
+    public class ControlaValor
+    {
+        public int Antecessor(Valor valor)
+        {
+            int antecessor = valor.Numero - 1;
+            return antecessor;
+        }
+
+        public int Sucessor(Valor valor)
+        {
+            int antecessor = valor.Numero + 1;
+            return antecessor;
+        }
+    }
+}
---
+++ Estudo4/Program.cs
@@ -0,0 +1,14 @@
+﻿using Estudo4.Control;
+using Estudo4.Entidade;
+
+Valor valor = new Valor();
+valor.Numero = 10;
+
+Console.WriteLine("Numero: " + valor.Numero);
+
+
+ControlaValor controla = new ControlaValor();
+int antecessor = controla.Antecessor(valor);
+int sucessor = controla.Sucessor(valor);
+Console.WriteLine("Antecessor: " + antecessor);
+Console.WriteLine("Sucessor: " + sucessor);
