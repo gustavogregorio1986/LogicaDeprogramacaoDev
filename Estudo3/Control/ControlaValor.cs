95f85845bd2c87b6b9eb115646bb5e6b4abebec3
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 17:27:43 -0300

    Crescendo no desnevolvolvimenro de sistemas para praticar a logica de programação

---
+++ Estudo3/Control/ControlaValor.cs
@@ -0,0 +1,17 @@
+﻿using Estudo3.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo3.Control
+{
+    public class ControlaValor
+    {
+        public int DobroDoValor(Valor valor)
+        {
+            return valor.Numero * 2;
+        }
+    }
+}
---
+++ Estudo3/Program.cs
@@ -0,0 +1,8 @@
+﻿using Estudo3.Control;
+using Estudo3.Entidade;
+
+Valor valor = new Valor();
+Console.WriteLine("Digite um número:");
+valor.Numero = Convert.ToInt32(Console.ReadLine());
+ControlaValor controla = new ControlaValor();
+Console.WriteLine($"O dobro do valor é: {controla.DobroDoValor(valor)}");
