8eafd7d5a335ca8d3e88dbf4de437441e63656a1
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 17:44:06 -0300

    Desenvolver logica de rogramação

---
+++ Estudo03/Control/ControlaValor.cs
@@ -0,0 +1,24 @@
+﻿using Estudo03.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo03.Control
+{
+    public class ControlaValor
+    {
+        public string VericarParImpar(Valor valor)
+        {
+            if (valor.Numero % 2 == 0)
+            {
+                return "Par"; // Par
+            }
+            else
+            {
+                return "Impar"; // Ímpar
+            }
+        }
+    }
+}
---
+++ Estudo03/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Estudo03.Control;
+using Estudo03.Entidade;
+
+var numero = new Valor();
+Console.WriteLine("Digite um número inteiro:");
+numero.Numero = Convert.ToInt32(Console.ReadLine());
+var controla = new ControlaValor();
+var resultado = controla.VericarParImpar(numero);
+Console.WriteLine(resultado);
