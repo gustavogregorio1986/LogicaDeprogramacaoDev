51aeadca5c6913c2d3ececc30fcc5a560c626626
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 17:02:34 -0300

    Estudando o recruso da linguagem paare voluir como profissional!

---
+++ Estudo01/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numeros = { 78, 32, 45, 32, 12, 76, 98 };
+
+IEnumerable<int> query = from numero in numeros
+                             where numero > 50
+                             orderby numero descending
+                             select numero;
+
+foreach (var i in query)
+{
+    Console.Write(i + " ");
+}
---
+++ Estudo1/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numeros = { 0, 1, 2, 3, 4, 5, 6 };
+
+var query = from numero in numeros
+            where numero % 2 == 0
+            select numero;
+
+foreach(int num in query)
+{
+       Console.Write("{0,1} ", num);
+
+}
