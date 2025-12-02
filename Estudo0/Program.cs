797b928bf43f4d4d7c58d4a739d89644a8bef52d
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-02 15:38:26 -0300

    Lofgica, aprnednedo do basico paar ser ume xcelente profissional

---
+++ Estudo0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Estudo0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This line prints "Hello, World"
+            Console.WriteLine("Hello World!");
+
+            /*
+             * Deveria ter saido daquele curso,  e focado mais no senac memso
+             * pois os enac deu mais retorno na minha vida profissional não deveria 
+             * ter atrasado minha vida naquele curso,d everia ter focado no senac
+             * senac dava mais retorno e podeira estar indo longe na minha vida
+             */
+        }
+    }
+}
